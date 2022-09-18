cfe93bc8acfa12bec6290972e28450869e9cb66b
mohkan1 <mohkan.998750@gmail.com>
2022-09-18 20:36:32 +0200

    Update js.js

--- js.js
+++ js.js
@@ -232,23 +232,4 @@ $( document ).ready(function() {
 
 
 
-for (var i = 0; i < 100000; i++) {  
-  myNetwork.activate([0,0]);  
-  myNetwork.propagate(learningRate, [0]);
-
-  myNetwork.activate([0,1]);  
-  myNetwork.propagate(learningRate, [1]);
-
-  myNetwork.activate([1,0]);  
-  myNetwork.propagate(learningRate, [1]);
-
-  myNetwork.activate([1,1]);  
-  myNetwork.propagate(learningRate, [0]);  
-}
-
-  console.log(myNetwork.activate([0,0]));   
-  console.log(myNetwork.activate([0,1]));  
-  console.log(myNetwork.activate([1,0]));  
-  console.log(myNetwork.activate([1,1]));
-
 });
